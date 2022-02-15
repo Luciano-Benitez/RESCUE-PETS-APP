169eb9cb0f3c4feecf6dd937ed9bd2aa0c654f9e
Luciano-Benitez <lbenitez19@hotmail.com>
2022-02-15 18:49:47 -0300

    Back:  Creacion de Carpetas Moduls y Routes.

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
+const {DB_NAME,
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rescuePets`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });

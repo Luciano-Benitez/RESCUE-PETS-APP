4340060321dddd2ea44a8217e49bdd9404476077
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-16 13:32:36 -0500

    update...

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,45 +1,13 @@
 import React, {Fragment} from "react";
-<<<<<<< HEAD
-import Colors from "../variables/Corlos"
-import {Link } from "react-router-dom";
-=======
 // import Colors from "../variables/Corlos"
 import {Link} from "react-router-dom";
 import {StyleNavBar, StyleLi , Divmenu,Enlacesmenu } from "../variables/Styles";
 
->>>>>>> b1f22623e28c11cfd202a6a17e08aa1baaf74019
 export default function Navbar() {
-
-
     return (
-        <header >
-            <div  style={{
-        backgroundColor:'#23282E',
-        
-      }}>
+        <Fragment>
+            <header>
               
-<<<<<<< HEAD
-            </div>
-            <nav >
-                <ul>
-                    <li>
-                        <Link to="/">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to="/">Soy Un Refugio</Link>
-                    </li>
-
-
-                    <li>
-                        <Link to="/nosotros">Nosotros</Link>
-                    </li>
-
-                   
-
-                 </ul>
-</nav>
-        </header>
-=======
                     <StyleNavBar>
                         <ul>
                         <Divmenu>
@@ -57,6 +25,5 @@ export default function Navbar() {
                 
             </header>
         </Fragment>
->>>>>>> b1f22623e28c11cfd202a6a17e08aa1baaf74019
     );
 }

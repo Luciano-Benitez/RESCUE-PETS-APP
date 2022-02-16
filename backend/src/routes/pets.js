bf74c6cf0a8cf4ef61e0e6ee24715814a028b0fc
[oriirigo] <[orii.irigo@gmail.com]>
2022-02-16 07:18:39 -0300

    Back: filtrar pets por temperamento

--- backend/src/routes/pets.js
+++ backend/src/routes/pets.js
@@ -1,19 +1,17 @@
-const express = require('express')
-const { addPet } = require('../controllers/addPet.js')
-const { filterTemperament } = require('../controllers/filterTemperament.js')
-const router = express.Router()
+
+const express = require("express");
+const { addPet } = require("../controllers/addPet.js");
+const router = express.Router();
+const { petsIdShelter } = require("../controllers/petsidshelters.js");
+const { getPets } = require("../controllers/getPets.js");
 const {filterTemperament} = require('../controllers/filterTemperament.js')
 
-router.get('/pets/:idShelters', async (req,res)=>{
-    const {idShelters} = req.params
-    if(idShelters){
-        return res.status(200).json(await petsIdShelter(idShelters))
-    }else{
-        return res.status(400)
-    }
-})
+router.get("/pets", getPets);
 
-router.post('/pets', addPet)
+router.get("/pets/:idShelter", petsIdShelter);
+
+router.post("/pets", addPet);
+
 router.get("/pets/:idTemperament", filterTemperament);
 
-module.exports = router
+module.exports = router;

979dfdf516c29641904bd79745c5c7a24eaff0e5
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-17 17:53:25 -0500

    Home update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,8 +7,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FormShelter from "./components/FormShelter";
 import {Home} from "./components/Home";
-import Header from "./components/Header";
-import Cards from "./components/Cards";
+
 import PreFooter from "./components/PreFooter";
 
 
@@ -16,9 +15,8 @@ function App() {
   return (
     <BrowserRouter>
        <Navbar></Navbar>
-      <Header></Header>
       <Home></Home>
-      <Cards></Cards>
+     
      
         <Routes>
           <Route path="/" element={<Home/>}/>
@@ -26,7 +24,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<FormShelter/>}/>
         </Routes>
-        
+
         <PreFooter/>
       <Footer />
     </BrowserRouter>
--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,12 +1,17 @@
 import Filters from "./Filters"
+import Header from "./Header";
+import Cards from "./Cards";
 
 export function Home(){
     return(
         <div>
+            <Header></Header>
             <h1>Home</h1>
             <h3> This is another header</h3>
             <input name='Search'></input>
+            
             <Filters />
+            <Cards></Cards>
         </div>
     )
 }

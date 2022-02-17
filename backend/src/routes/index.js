5ee7f777ef87393886a35d91e19eb23c79840a50
Luciano-Benitez <lbenitez19@hotmail.com>
2022-02-17 13:23:01 -0300

    Back: Función createShelter

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,10 @@ const router = Router();
 const axios = require("axios");
 const petsidshelter = require('./pets.js')
 const adoption = require('./form.js');
+const createShelter = require('./CreateLogsShelters');
 
 router.use('/',petsidshelter)
 router.use('/',adoption)
+router.use('/',createShelter)
 
 module.exports = router;

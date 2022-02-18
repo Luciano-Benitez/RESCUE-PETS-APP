8a0ff51364240c3d60632e3fe6a7c0238d7aa09c
[oriirigo] <[orii.irigo@gmail.com]>
2022-02-18 02:22:09 -0300

    back :ruta pet por id

--- backend/src/routes/pets.js
+++ backend/src/routes/pets.js
@@ -5,6 +5,9 @@ const { petsIdShelter } = require("../controllers/petsidshelters.js");
 const { getPets } = require("../controllers/getPets.js");
 const { filterTemperament } = require("../controllers/filterTemperament.js");
 const { postVaccines } = require("../controllers/petsVaccines");
+const {petsId}= require("../controllers/petsId");
+const { getAllPetsinDB } = require("../controllers/getAllPetsinDB.js");
+
 
 router.get("/pets/:idCity", getPets);
 
@@ -15,5 +18,20 @@ router.post("/pets", addPet);
 // router.get("/pets/:idTemperament", filterTemperament);
 
 router.post("/postVaccines", postVaccines);
+
+router.get('/petDetail/:idPets',petsId )
+
+router.get("/petDetail", async (req, res) => {
+    
+    let allPets = await getAllPetsinDB();
+   
+      if (allPets.length > 0) {
+        res.status(200).send(allPets);
+      } else {
+        res.status(400).json("Sorry,pet not found");
+      }
+   
+  });
+
 
 module.exports = router;

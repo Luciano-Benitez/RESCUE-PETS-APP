f7f1155ff9911e74aff63d4d68d90dad49bf1b60
AilenMz <ailen.martinez@usal.edu.ar>
2022-02-18 20:56:40 -0300

    ruta country

--- backend/src/routes/country.js
+++ backend/src/routes/country.js
@@ -3,17 +3,26 @@ const express = require('express')
 const router = express.Router()
 
 const postCountry = require('../controllers/postCountry')
+const countries = require('../utils/countries.json')
 
 router.post('/country',postCountry);
 
 
 router.get('/country', async (req,res)=>{
-    let allCountries = await Countries.findAll();
-    if (allCountries){
-        res.status(200).send(allCountries)
-    } else {
-        res.status(400).json('Sorry, there is no Countries')
-    }
+
+    let data = countries.countries
+
+    data.forEach(element => {
+        Countries.findOrCreate({
+            where: {
+                id : element.id,
+                country: element.name
+            }
+        })
+    });
+    const AllCountries = await Countries.findAll()
+
+    res.status(200).send(AllCountries)
 });
 
 module.exports = router

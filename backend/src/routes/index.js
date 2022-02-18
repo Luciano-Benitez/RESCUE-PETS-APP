992d7168a071635999a146e7c7a8c1df512a2a69
AilenMz <ailen.martinez@usal.edu.ar>
2022-02-18 11:42:18 -0300

    countrues

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,10 +5,12 @@ const pets = require('./pets.js')
 const form = require('./form.js');
 const roles = require('./roles')
 const createShelter = require('./CreateLogsShelters');
+const countries = require('./country')
 
 router.use('/',pets)
 router.use('/',form)
 router.use('/',createShelter)
 router.use('/',roles)
+router.use('/',countries)
 
 module.exports = router;

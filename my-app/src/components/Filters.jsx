2cf06f94c04fed2bc28bd82c2dc61220c1c09a66
alexisonfiremx <themarkvolta@gmail.com>
2022-02-20 12:50:20 -0800

    se creo action para traer temperamentos y renderizarlos como opciones en filtro

--- my-app/src/components/Filters.jsx
+++ my-app/src/components/Filters.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {getcities, getCountries, getPetsFilter, getStates} from '../Redux/Actions/index'
+import {getcities, getCountries, getPetsFilter, getStates, getTemperaments} from '../Redux/Actions/index'
 import { Container,SelectStyle } from '../Styles/StyledFilters'
 
 const Filters = ({idcity}) => {
@@ -14,9 +14,14 @@ const Filters = ({idcity}) => {
      const states = useSelector((state) => state.states)
      const cities = useSelector((state) => state.cities)
      const pets = useSelector((state) => state.petsfilter)
+     const temperaments = useSelector((state) => state.temperaments)
 
      useEffect(()=>{
           dispatch(getCountries())
+     },[dispatch])
+
+     useEffect(()=>{
+          dispatch(getTemperaments())
      },[dispatch])
 
 
@@ -40,67 +45,67 @@ const Filters = ({idcity}) => {
           dispatch(getPetsFilter(query))
      }
 
-  return (
-    <Container>
-        {/* <label>Por País:</label> */}
-            <SelectStyle onChange={e => handleSubmitCountry(e)}>
-                 <option hidden >Países</option>
-                 {countries.map(e => (
-                      <option key={e.id} value={e.id} >{e.country}</option>
-                 ))}
-            </SelectStyle>
-        {/* <label>Por Ciudades:</label> */}
-           <SelectStyle onChange={e => handleSubmitState(e)}>
-                <option hidden >Estados</option>
-                {states.map(e => (
-                     <option key={e.id} value={e.id} >{e.state}</option>
-                ))}
-           </SelectStyle>
+     return (
+     <Container>
+          {/* <label>Por País:</label> */}
+               <SelectStyle onChange={e => handleSubmitCountry(e)}>
+                    <option hidden >Países</option>
+                    {countries.map(e => (
+                         <option key={e.id} value={e.id} >{e.country}</option>
+                    ))}
+               </SelectStyle>
+          {/* <label>Por Ciudades:</label> */}
+               <SelectStyle onChange={e => handleSubmitState(e)}>
+                    <option hidden >Estados</option>
+                    {states.map(e => (
+                         <option key={e.id} value={e.id} >{e.state}</option>
+                    ))}
+               </SelectStyle>
 
-           <SelectStyle onChange={e => handleSubmitCities(e)}>
-                <option hidden >Ciudades</option>
-                {cities.map(e => (
-                     <option key={e.id} value={e.id} >{e.city}</option>
-                ))}
-           </SelectStyle>
+               <SelectStyle onChange={e => handleSubmitCities(e)}>
+                    <option hidden >Ciudades</option>
+                    {cities.map(e => (
+                         <option key={e.id} value={e.id} >{e.city}</option>
+                    ))}
+               </SelectStyle>
 
-        {/* <label>Por Refugio:</label> */}
-       <SelectStyle>
-                <option hidden >Refugios</option>
-                <option>Mock-up: Refugio 1</option>
-                <option>Mock-up: Refugio 2</option>
-                <option>Mock-up: Refugio 3</option>
-           </SelectStyle>
-        {/* <label>Por Especie:</label> */}
-       <SelectStyle>
-                <option hidden >Especies</option>
-                <option>Mock-up: Perros</option>
-                <option>Mock-up: Gatos</option>
-                <option>Mock-up: Otros</option>
-           </SelectStyle>
-        {/* <label>Por Edad:</label> */}
-       <SelectStyle>
-                <option hidden >Rango Edad</option>
-                <option>Mock-up: Rango 1 </option>
-                <option>Mock-up: Rango 2</option>
-                <option>Mock-up: Rango 3</option>
-           </SelectStyle>
-        {/* <label>Temperamento:</label> */}
-       <SelectStyle>
-                <option hidden >Temperamento</option>
-                <option>Mock-up: Temp 1 </option>
-                <option>Mock-up: Temp 2</option>
-                <option>Mock-up: Temp 3</option>
-           </SelectStyle>
-        {/* <label>Status:</label> */}
-       <SelectStyle onChange={(e)=>handleStatus(e)}>
-                <option hidden >Status</option>
-                <option>Mock-up: Sin adoptar </option>
-                <option>Mock-up: En proceso </option>
-                <option>Mock-up: Adoptado</option>
-           </SelectStyle>
-    </Container>
-  )
+          {/* <label>Por Refugio:</label> */}
+          <SelectStyle>
+                    <option hidden >Refugios</option>
+                    <option>Mock-up: Refugio 1</option>
+                    <option>Mock-up: Refugio 2</option>
+                    <option>Mock-up: Refugio 3</option>
+               </SelectStyle>
+          {/* <label>Por Especie:</label> */}
+          <SelectStyle>
+                    <option hidden >Especies</option>
+                    <option>Mock-up: Perros</option>
+                    <option>Mock-up: Gatos</option>
+                    <option>Mock-up: Otros</option>
+               </SelectStyle>
+          {/* <label>Por Edad:</label> */}
+          <SelectStyle>
+                    <option hidden >Rango Edad</option>
+                    <option>Mock-up: Rango 1 </option>
+                    <option>Mock-up: Rango 2</option>
+                    <option>Mock-up: Rango 3</option>
+               </SelectStyle>
+          {/* <label>Temperamento:</label> */}
+          <SelectStyle>
+                    <option hidden >Temperamento</option>
+                    {temperaments?.map(e => (
+                         <option key={e.id} value={e.id} >{e.temperament}</option>
+                    ))}
+               </SelectStyle>
+          {/* <label>Status:</label> */}
+          <SelectStyle onChange={(e)=>handleStatus(e)}>
+                    <option hidden >Status</option>
+                    <option>Mock-up: Sin adoptar </option>
+                    <option>Mock-up: En proceso </option>
+                    <option>Mock-up: Adoptado</option>
+               </SelectStyle>
+     </Container>
+     )
 }
 
 export default Filters

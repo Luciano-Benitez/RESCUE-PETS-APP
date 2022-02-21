fe4ee213ab9e44439ed6c0fb6f6421711cad453d
AilenMz <ailen.martinez@usal.edu.ar>
2022-02-21 15:08:52 -0300

    dashboard

--- my-app/src/components/Dashboard.jsx
+++ my-app/src/components/Dashboard.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const Dashboard = () => {
   return (
-    <div>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-      <h1>Aqui iria el Dashboard</h1>
-    </div>
+    <DivContainer>
+      <img src="https://res.cloudinary.com/djbiam1gm/image/upload/v1645461682/WhatsApp_Image_2022-02-21_at_8.40.33_AM_xzhaq2.jpg" alt="img" />
+    </DivContainer>
     
   )
 }
+
+const DivContainer = styled.div`
+  width: 100vw;
+  height: 80vh;
+  margin-top: 10rem;
+  display: flex;
+  justify-content: center;
+`

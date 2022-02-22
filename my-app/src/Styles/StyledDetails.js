4fdb6e3c709543c16e9b64190d78b1c180165bba
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-22 13:38:32 -0500

    Details pets Style

--- my-app/src/Styles/StyledDetails.js
+++ my-app/src/Styles/StyledDetails.js
@@ -1,46 +1,78 @@
 import styled from "styled-components";
 import allColors from "../variables/Colors";
+import Img from "../Icos/wave2.svg"
 
-export const StyledDetails = styled.div` //Contenedor
+export const StyledDetails = styled.div`
+  //Contenedor
   height: 100%;
-
-  background-color: ${allColors.colors[8]};
-  border-radius: 4px;
-  padding-top: 1em;
-  transition: top ease 0.5s;
-  overflow: hidden;
-  border-radius: 10px;
-
-
+  display: flex;
+  color: ${allColors.colors[8]};
+  align-items: center;
+  justify-content: space-around;
+  background-image: url(${Img});
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-top: 120px;
+  padding :40px;
   
 `;
 
+export const StyledDetailsLeft = styled.div`
+  
+  width: 50%;
+  border-radius: 9px;
+ margin:0;
+  font-size: 35px;
+  text-align: center;
+ 
+ border-radius: 12px;
+  h1 {
+    color: ${allColors.colors[8]};
+    font-size: 12px !important;
+  }
+`;
+
+export const StyledDetailsRight = styled.div`
+background-color: white;
+
+  width: 50%;
+  padding: 18px;
+  margin:16px;
+  box-shadow: 0 0 15px #B8B9BA;
+  border-radius: 12px;
+  h2 {
+    color: ${allColors.colors[8]};
+    font-size: 12px !important;
+  }
+
+  h1 {
+    color: ${allColors.colors[2]};
+    font-size: 11px !important;
+    display:inline;
+  }
+  h3 {
+    text-align: center;
+    color: ${allColors.colors[8]};
+    font-size: 20px !important;
+  }
+
+  p{
+font-size : 15px;
 
 
-export const StyledDetailsLeft = styled.div` 
-  height: 330% ;
-  width: 285px;
-  background-color: ${allColors.colors[3]};
-  border-radius: 4px;
-
-  h1{
-    color:  ${allColors.colors[8]};
-    font-size: 26;
   }
   
 `;
 
 
+export const Cuadro = styled.div`
+ width: 100%;
+ height:470px;
+ overflow: hidden;
+ border-radius: 12px;
 
+`
 
-export const StyledDetailsRight = styled.div` 
-  height: 330% ;
-  width: 285px;
-  background-color: ${allColors.colors[4]};
-  border-radius: 4px;
-
-  h2{
-    color:  ${allColors.colors[8]};
-    font-size: 16;
-  }
-`;
+export const Imgag = styled.img`
+  width: 130%;
+`

ebf8e32a9523f98277fa4926f2c7d2c9d8d25551
alexisonfiremx <themarkvolta@gmail.com>
2022-02-21 22:23:58 -0800

    se creo ruta del back para conseguir shelterId y cityId, junto con su action creator en front

--- my-app/src/components/PetsInDashboard.jsx
+++ my-app/src/components/PetsInDashboard.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
+import { getIdFromShelterAndCity } from '../Redux/Actions'
 
 const PetsInDashboard = () => {
 
     const idUser = useSelector(state => state.id)
     console.log(idUser)
+    
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+    dispatch(getIdFromShelterAndCity(idUser))
+    },[dispatch])
+    
+    const shelterData = useSelector(state => state.ShelterAndCityId)
+    console.log(shelterData)
 
   return (
     <Center>

f609143a6561c1b7e09fee28c81cac9bc2e6c30d
AilenMz <ailen.martinez@usal.edu.ar>
2022-02-23 19:15:14 -0300

    filter

--- my-app/src/components/ShelterInfo.jsx
+++ my-app/src/components/ShelterInfo.jsx
@@ -1,34 +1,48 @@
-import React, {Fragment} from 'react'
-import FiltersInShelterDetails from './FiltersInShelterDetails'
+import React, { Fragment } from "react";
+import FiltersInShelterDetails from "./FiltersInShelterDetails";
 
 // estilos
-import {DivContenedor} from '../Styles/StyledShelterDetails'
-import {StyledCard, StyledCardContainer, ImgCard} from '../Styles/StyledCards.js';
+import { DivContenedor } from "../Styles/StyledShelterDetails";
+import {
+  StyledCard,
+  StyledCardContainer,
+  ImgCard,
+} from "../Styles/StyledCards.js";
 
-
-const ShelterInfo = ({Data, pets, input, setInput}) =>{
- 
-    return (
+const ShelterInfo = ({ Data, pets, input, setInput }) => {
+  return (
+    <div>
+      <h1>Hola, soy la info del refugio</h1>
+      <DivContenedor>
+        <FiltersInShelterDetails input={input} setInput={setInput} />
         <div>
-            <h1>Hola, soy la info del refugio</h1>
-            <DivContenedor>
-                <FiltersInShelterDetails input={input} setInput={setInput}/>
-                <div>
-                <StyledCardContainer key={Math.random(5)}>
-                    {
-                        pets ? pets.map(e => (<Fragment key={e.id} >
-                            <StyledCard >
-                               <h1>{e.name}</h1>
-                               <ImgCard src={e.image}/>
-                            </StyledCard>
-                            </Fragment> )) : <h2>Loading...</h2>
-                    } 
-                </StyledCardContainer>    
-                </div>
-            </DivContenedor>
+          <StyledCardContainer key={Math.random(5)}>
             
-        </div>
-    )
-}
+            {typeof pets !== "string" ? (
+              pets.map((p) => (
+                <Fragment key={p.id}>
+                  <Link to={`/details/${p.id}`}>
+                    <StyledCard>
+                      <h1>{p.name}</h1>
 
-export default ShelterInfo
+                      {/* <p>{p.description}</p> */}
+                      <ImgCard src={p.image} />
+
+                      <br />
+                    </StyledCard>
+                  </Link>
+                </Fragment>
+              ))
+            ) : typeof pets === "string" ? (
+              <h1> {pets}</h1>
+            ) : (
+              <h1> Cargando datos</h1>
+            )}
+          </StyledCardContainer>
+        </div>
+      </DivContenedor>
+    </div>
+  );
+};
+
+export default ShelterInfo;

31acfe3d75b011557ef0446d7715cf5091780c8d
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-22 19:06:03 -0500

    Details Update

--- my-app/src/Styles/StyledDetails.js
+++ my-app/src/Styles/StyledDetails.js
@@ -12,7 +12,7 @@ export const StyledDetails = styled.div`
   background-image: url(${Img});
   background-repeat: no-repeat;
   background-size: cover;
-  margin-top: 120px;
+  margin-top: 80px;
   padding :40px;
   
 `;
@@ -30,6 +30,7 @@ export const StyledDetailsLeft = styled.div`
     color: ${allColors.colors[8]};
     font-size: 12px !important;
   }
+ 
 `;
 
 export const StyledDetailsRight = styled.div`
@@ -37,23 +38,28 @@ background-color: white;
 
   width: 50%;
   padding: 18px;
-  margin:16px;
+  margin:19px;
   box-shadow: 0 0 15px #B8B9BA;
   border-radius: 12px;
   h2 {
     color: ${allColors.colors[8]};
-    font-size: 12px !important;
+    font-size: 21px !important;
+  }
+  span {
+    color: ${allColors.colors[2]};
+    font-size: 13px !important;
   }
 
   h1 {
     color: ${allColors.colors[2]};
-    font-size: 11px !important;
-    display:inline;
+    font-size: 15px !important;
+    text-align: center;
+   
   }
   h3 {
     text-align: center;
-    color: ${allColors.colors[8]};
-    font-size: 20px !important;
+    color: ${allColors.colors[3]};
+    font-size: 33px !important;
   }
 
   p{
@@ -67,7 +73,7 @@ font-size : 15px;
 
 export const Cuadro = styled.div`
  width: 100%;
- height:470px;
+ height:445px;
  overflow: hidden;
  border-radius: 12px;
 

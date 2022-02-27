c61490abca314e9e233fade53c181da92c515e16
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-27 03:12:04 -0500

    Admin  UPDATE...

--- my-app/src/Styles/StyledDashboard.js
+++ my-app/src/Styles/StyledDashboard.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import allColors from "../variables/Colors";
-import Img from "../Icos/wave2.svg"
+import Img from "../Icos/wave2.svg";
 
-export const StyledDetails = styled.div`
+export const Container = styled.div`
   //Contenedor
   height: 100%;
   display: flex;
@@ -13,34 +13,52 @@ export const StyledDetails = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   margin-top: 80px;
-  padding :40px;
+  padding: 40px;
+`;
+
+export const Left = styled.div`
+  width: 30%;
+  padding: 8px;
   
 `;
 
-export const StyledDetailsLeft = styled.div`
-  
-  width: 50%;
-  border-radius: 9px;
- margin:0;
-  font-size: 35px;
+export const LeftMini = styled.div`
+  box-shadow: 0 0 7px #b8b9ba;
+  border-radius: 7px;
+margin-top: 1em;
+  font-size: 14px;
   text-align: center;
- 
- border-radius: 12px;
+  line-height: 40px;
+  background-color: white;
+  height: 40px;
+  overflow: hidden; 
+cursor: pointer;
+  
+  &:hover {
+  top: 1px;
+  background-color:${allColors.colors[8]};
+  color: #ffffff;
+
+
+
+  border-radius: 7px;
+  box-shadow: 0 0 7px #b8b9ba;
+
+}
+
   h1 {
     color: ${allColors.colors[8]};
     font-size: 12px !important;
   }
- 
 `;
 
-export const StyledDetailsRight = styled.div`
-background-color: white;
-
-  width: 50%;
-  padding: 18px;
-  margin:19px;
-  box-shadow: 0 0 15px #B8B9BA;
+export const Right = styled.div`
+  width: 70%;
+  padding: 48px;
+  margin: 19px;
+  box-shadow: 0 0 15px #b8b9ba;
   border-radius: 12px;
+  background-color: white;
   h2 {
     color: ${allColors.colors[8]};
     font-size: 18px !important;
@@ -48,14 +66,12 @@ background-color: white;
   span {
     color: ${allColors.colors[2]};
     font-size: 13px !important;
-    
   }
 
   h1 {
     color: ${allColors.colors[2]};
     font-size: 15px !important;
     text-align: center;
-   
   }
   h3 {
     text-align: center;
@@ -63,23 +79,13 @@ background-color: white;
     font-size: 33px !important;
   }
 
-  p{
-font-size : 15px;
-
-
+  p {
+    font-size: 15px;
   }
-  
+`;
+
+export const img1 = styled.img`
+  width: 130%;
 `;
 
 
-export const Cuadro = styled.div`
- width: 100%;
- height:525px;
- overflow: hidden;
- border-radius: 12px;
-
-`
-
-export const Imgag = styled.img`
-  width: 130%;
-`
